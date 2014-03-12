23a63d298fa23e6d587522eb91d04722b30d7c36
monciwoda <monciwoda@outlook.com>
2014-03-12 09:17:26 +0100

    zmiany

--- main.py
+++ main.py
@@ -2,4 +2,23 @@ from rail import rail, derail
 
 
 if __name__ == '__main__':
-    pass
+    menu = 99
+    while True:
+        if menu == 99:
+            print('Wybierz operacje do wykonania:\n'
+                  '1. rail\n'
+                  '2. derail\n'
+                  '7. szyfrowanie cezara\n'
+                  '8. deszyfrowanie cezara\n'
+                  '99. Wyswietlenie opcji\n'
+                  'ctrl-c. wyjscie\n')
+        menu = 0
+        temp = input('> ')
+        try:
+            menu = int(temp)
+        except ValueError:
+            print('blad!')
+        except KeyboardInterrupt:
+            print('wyjscie')
+            break
+        if menu == 7:
--- rail/rail.py
+++ rail/rail.py
@@ -1,2 +1,13 @@
 def rail(tekst='', en=1):
-    pass
+    out = ''
+    if en == 1:
+        return tekst
+    for i in range(en):
+        l_dol = list(range(i, len(tekst), (2 * en) - 2))
+        l_gora = list(range((2 * en) - 2 - i, len(tekst), (2 * en) - 2))
+        lista = list(set(l_dol + l_gora))
+        lista.sort()
+        for it in lista:
+            out += tekst[it]
+    return out
+
