55c63f8247a09d92c1998a0b0a2a2557a6203784
fisz <grafiszti@gmail.com>
2014-03-16 12:38:15 +0100

    zrobiony Vigeneren dodane opcje do menu, cesar działa szyfrowanie a deszyfrowanie nie, pracuje nad tym

--- cesar/decesar.py
+++ cesar/decesar.py
@@ -1,6 +1,22 @@
 import constants
 
 
+def fme(a, k, n):
+    b = bin(k)[2:] # list of bits
+    m = len(b)
+    r = 1 # result
+    x = a % n
+
+    for i in range(m - 1, -1, -1):
+        if b[i] == '1':
+            r = r * x % n
+
+        x **= 2
+        x %= n
+
+    return r
+
+
 def hcf(n1, n2):
   while n1*n2:
     if n1 > n2:
@@ -28,9 +44,9 @@ def decrypt_cesar(tekst='', k0=0, k1=0):
     for i in range(len(tekst)):
         if tekst[i] in constants.alphabet:
             temp = constants.alphabet.index(tekst[i])
-            out += constants.alphabet[pow(((temp + len(constants.alphabet) - k0) * k1),
-                                          (euler_number - 1),
-                                          len(constants.alphabet))]
+            out += constants.alphabet[fme(((temp + len(constants.alphabet) - k0) * k1),
+                                              (euler_number - 1),
+                                              len(constants.alphabet))]
         else:
             out += ' '
     return out
