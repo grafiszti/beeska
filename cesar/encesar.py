d538140f01896ac92db625f9786f35e1d8ea759d
fisz <grafiszti@gmail.com>
2014-03-16 22:12:33 +0100

    dziala caly cesar i vigenere, moja robota skonczona

--- cesar/encesar.py
+++ cesar/encesar.py
@@ -4,5 +4,8 @@ import constants
 def encrypt_cesar(tekst, k0, k1):
     out = ''
     for i in range(len(tekst)):
-        out += constants.alphabet[((constants.alphabet.index(tekst[i]) * k1) + k0) % len(constants.alphabet)]
+        if tekst[i] in constants.alphabet:
+            out += constants.alphabet[((constants.alphabet.index(tekst[i]) * k1) + k0) % len(constants.alphabet)]
+        else:
+            out += ' '
     return out
